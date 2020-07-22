e3cb3a8b890d5f8e111f2beda46ea1fc11ff5640
TarequeBashar <ovitareque@gmail.com>
2020-07-22 21:04:43 +0600

    Final

---
+++ Box Shooter By Ovi/Assets/Scripts/BasicController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Public Variables 
+
+    public float moveSpeed = 3.0f;
+    public float gravity = 9.81f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Box Shooter By Ovi/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Define possible states of motion through enum data type
+
+    public enum motionDirections {Spin , Horizontal , Vertical};
+
+    // Store the state
+
+    public motionDirections motionState = motionDirections.Horizontal;
+
+    // Motion parameter
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        switch (motionState)
+        {
+
+            case motionDirections.Spin:
+                // rotate around the up axix of the gameObject
+
+                gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+
+                break;
+
+
+            case motionDirections.Vertical:
+
+                // move up and down over time
+
+                gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+
+
+
+                break;
+
+            case motionDirections.Horizontal:
+                // move up and down over time
+                gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+    }
+}
+
+
+        
